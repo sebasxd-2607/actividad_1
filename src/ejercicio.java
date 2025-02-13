f1ff802b91d03a82935b2ab6b3f743c1af19b5d0
sebasxd-2607 <jseba2105.2007@gmail.com>
2025-02-13 17:41:16 -0500

    nombre y ciudad

---
+++ src/ejercicio.java
@@ -0,0 +1,8 @@
+
+public class ejercicio {
+    public static void main(String[] args) {
+    System.out.println( "juan sebastian rodriguez ");
+    System.out.println(("zipaquira"));
+
+        }
+    }
